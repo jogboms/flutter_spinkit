95b53015314791b16897f3b7e3a407b36514aa46
Jogboms <jeremiahogbomo@gmail.com>
2018-06-25 22:35:43 +0100

    feat: SpinKitPulse

---
+++ lib/src/pulse.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class SpinKitPulse extends StatefulWidget {
+  final Color color;
+  final double width;
+  final double height;
+
+  SpinKitPulse({
+    Key key,
+    @required this.color,
+    this.width = 50.0,
+    this.height = 50.0,
+  }) : super(key: key);
+
+  @override
+  _SpinKitPulseState createState() => new _SpinKitPulseState();
+}
+
+class _SpinKitPulseState extends State<SpinKitPulse> with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+  Animation<double> _animation;
+
+  @override
+  initState() {
+    super.initState();
+    _controller = new AnimationController(vsync: this, duration: Duration(seconds: 1));
+    _animation = CurveTween(curve: Curves.easeInOut).animate(_controller)
+      ..addListener(
+        () => setState(() => {}),
+      );
+
+    _controller.repeat();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Opacity(
+        opacity: 1.0 - _animation.value,
+        child: new Transform.scale(
+          scale: _animation.value,
+          child: new Container(
+            height: widget.height,
+            width: widget.width,
+            decoration: BoxDecoration(shape: BoxShape.circle, color: widget.color),
+          ),
+        ),
+      ),
+    );
+  }
+}
