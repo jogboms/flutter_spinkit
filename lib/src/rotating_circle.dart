dcfaaaea9639ea0476053ff7c651992a2ad8b970
Jogboms <jeremiahogbomo@gmail.com>
2018-06-26 00:07:56 +0100

    fix: rotating circle transform

--- lib/src/rotating_circle.dart
+++ lib/src/rotating_circle.dart
@@ -24,17 +24,19 @@ class _SpinKitRotatingCircleState extends State<SpinKitRotatingCircle> with Sing
   @override
   initState() {
     super.initState();
-    _controller = new AnimationController(vsync: this, duration: Duration(milliseconds: 1000));
-    _animation1 = CurveTween(curve: Curves.easeInOut).animate(
+    _controller = new AnimationController(vsync: this, duration: Duration(milliseconds: 1200));
+    _animation1 = Tween(begin: 0.0, end: 180.0).animate(
       new CurvedAnimation(
         parent: _controller,
         curve: new Interval(0.0, 0.5, curve: Curves.easeIn),
       ),
     )..addListener(() => setState(() => {}));
 
-    _animation2 = new CurvedAnimation(
-      parent: _controller,
-      curve: new Interval(0.5, 1.0, curve: Curves.easeOut),
+    _animation2 = Tween(begin: 0.0, end: 180.0).animate(
+      new CurvedAnimation(
+        parent: _controller,
+        curve: new Interval(0.5, 1.0, curve: Curves.easeOut),
+      ),
     )..addListener(() => setState(() => {}));
 
     _controller.repeat();
@@ -48,7 +50,9 @@ class _SpinKitRotatingCircleState extends State<SpinKitRotatingCircle> with Sing
 
   @override
   Widget build(BuildContext context) {
-    final Matrix4 transform = new Matrix4.identity()..scale(1 - _animation2.value, _animation1.value, 1.0);
+    final Matrix4 transform = new Matrix4.identity()
+      ..rotateX((0 - _animation1.value) * 0.0174533)
+      ..rotateY((0 - _animation2.value) * 0.0174533);
     return Center(
       child: new Transform(
         transform: transform,
