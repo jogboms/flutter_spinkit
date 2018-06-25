e5dcebf0f9c0649bf8f9c9f8706b66cfd7d1be79
Jeremiah Ogbomo <jeremiahogbomo@gmail.com>
2018-06-26 00:25:56 +0100

    feat: rotating plane (#3)
    * feat: rotating square
    
    * fix: rotating circle transform
    
    * chore(README): update Roadmap
    
    * chore: code clean up
    
    * chore(example): workspace clean up

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -11,7 +11,7 @@ import 'package:example/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(new MyApp());
+    await tester.pumpWidget(new SpinKit());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
