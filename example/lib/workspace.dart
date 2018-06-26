74fb2c2ed61871a9e5bb4ff4c4ba363bbff1d9ed
Jeremiah Ogbomo <jeremiahogbomo@gmail.com>
2018-06-26 18:22:45 +0100

    chore: code clean up (#10)

---
+++ example/lib/workspace.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class WorkSpace extends StatefulWidget {
+  @override
+  WorkSpaceState createState() => new WorkSpaceState();
+}
+
+class WorkSpaceState extends State<WorkSpace> {
+  @override
+  Widget build(BuildContext context) {
+    return new Container();
+  }
+}
