5d2ffdc9cd333045523a25a4582456e1b8b2956b
Jogboms <jeremiahogbomo@gmail.com>
2018-06-28 16:02:28 +0100

    chore: WIP

--- example/lib/workspace.dart
+++ example/lib/workspace.dart
@@ -48,18 +48,55 @@ class _SpinKitPulseState extends State<SpinKitPulse> with SingleTickerProviderSt
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Opacity(
-        opacity: 1.0 - _animation.value,
-        child: new Transform.scale(
-          scale: _animation.value,
-          child: new Container(
-            height: widget.height,
-            width: widget.width,
-            decoration: BoxDecoration(shape: BoxShape.circle, color: widget.color),
+      child: SizedBox.fromSize(
+        size: Size.square(widget.height),
+        child: Stack(
+          children: [
+            _circle(3),
+            _circle(9),
+            // _circle(1),
+            // _circle(2),
+            // _circle(3),
+            // _circle(4),
+            // _circle(5),
+            // _circle(6),
+            // _circle(7),
+            // _circle(8),
+            // _circle(9),
+            // _circle(10),
+            // _circle(11),
+            // _circle(12),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _circle(int i) {
+    final _size = widget.width * 0.25;
+    final Matrix4 _tRotate = new Matrix4.identity()
+      // ..rotateX(30.0 * (i - 1) * 0.0174533)
+      // ..rotateY(30.0 * (i - 1) * 0.0174533)
+      ..rotateZ(30.0 * (i - 1) * 0.0174533);
+    // final Matrix4 _tRotate = new Matrix4.identity()..rotateY(30.0 * (i - 1) * 0.0174533);
+    // final Matrix4 _tRotate = Matrix4.rotationZ(30.0 * (i - 1) * 0.0174533);
+    // final Matrix4 _tRotate = Matrix4.rotate(30.0 * (i - 1) * 0.0174533);
+
+    return Positioned(
+      top: i + 0.0,
+      left: i + 0.0,
+      child: Transform(
+        transform: _tRotate,
+        alignment: FractionalOffset.center,
+        child: Container(
+          width: _size,
+          height: _size,
+          decoration: BoxDecoration(
+            color: widget.color,
+            shape: BoxShape.circle,
           ),
         ),
       ),
     );
   }
 }
-
